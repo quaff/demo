1b0f929efab806919ea16e9a1197c000e30924b2
Yanming Zhou <zhouyanming@gmail.com>
2017-08-02 19:51:12 +0800

    Clean unnecessary code

--- src/com/demo/enums/Status.java
+++ src/com/demo/enums/Status.java
@@ -7,16 +7,6 @@ public enum Status implements Displayable {
 	NORMAL, LOW_INVENTORY, SOLDOUT;
 
 	@Override
-	public String getName() {
-		return name();
-	}
-
-	@Override
-	public String getDisplayName() {
-		return Displayable.super.getDisplayName();
-	}
-
-	@Override
 	public String toString() {
 		return getDisplayName();
 	}
