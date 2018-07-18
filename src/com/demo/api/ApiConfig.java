10d3ddcdf002d7f39f66091b5c480fe841ff482e
Yanming Zhou <zhouyanming@gmail.com>
2018-07-18 19:00:27 +0800

    Upgrade

--- src/com/demo/api/ApiConfig.java
+++ src/com/demo/api/ApiConfig.java
@@ -3,10 +3,7 @@ package com.demo.api;
 import org.ironrhino.rest.ApiConfigBase;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 
-//only for exclude-filter of root ctx
-@ControllerAdvice
 @Configuration
 @ComponentScan
 public class ApiConfig extends ApiConfigBase {
