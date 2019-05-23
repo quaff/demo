00185e1f7915343ee78397b7cd4b047e2ddb06eb
Yanming Zhou <zhouyanming@gmail.com>
2019-05-23 15:51:20 +0800

    Improve User

--- src/org/ironrhino/security/model/User.java
+++ src/org/ironrhino/security/model/User.java
@@ -4,22 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.ironrhino.core.aop.PublishAware;
-import org.ironrhino.core.metadata.AutoConfig;
-import org.ironrhino.core.metadata.Richtable;
-import org.ironrhino.core.search.elasticsearch.annotations.Searchable;
-
 import com.demo.model.Department;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@PublishAware
-@AutoConfig
-@Searchable
 @Entity
 @Table(name = "user")
-@Richtable(order = "username asc")
 @Getter
 @Setter
 public class User extends BaseUser {
